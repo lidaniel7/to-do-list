c6eb2217345de20cb7980569e78d3aac952a2d40
Daniel Li <lidaniel225@gmail.com>
2020-07-08 11:23:19 -0700

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Todos from './Components/Todos.js';
 import './App.css';
 import Header from './Components/Header.js';
 import ToDoItem from './Components/ToDoItem.js';
