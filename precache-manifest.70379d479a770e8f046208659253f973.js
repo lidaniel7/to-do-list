148fa9c1d4b3d24017faec2b0ba4cb3e3346e0e2
Daniel Li <lidaniel225@gmail.com>
2020-07-17 12:25:29 -0700

    Updates

---
+++ precache-manifest.70379d479a770e8f046208659253f973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969d1e4158d24a8293c5235fcbb717e4",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "e3fa8e7295fb752acb39",
+    "url": "/to-do-list/static/css/main.e0b1d065.chunk.css"
+  },
+  {
+    "revision": "e7427694aa197f53391e",
+    "url": "/to-do-list/static/js/2.04e0af57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.04e0af57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fa8e7295fb752acb39",
+    "url": "/to-do-list/static/js/main.ed792f6c.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
